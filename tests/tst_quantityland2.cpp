6f343f0dd1f1cd7018fccd53b55df979d8a1130b
Anton Kreuzkamp <anton.kreuzkamp@kdab.com>
2019-05-28 00:27:32 +0200

    Correct License and add License.txt
    Changes the license from GPL to LGPL and adds a license file to the root
    directory.

--- tests/tst_quantityland2.cpp
+++ tests/tst_quantityland2.cpp
@@ -1,3 +1,23 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright 2019  Anton Kreuzkamp <anton.kreuzkamp@kdab.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file License.txt.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
 #include <quantityland2.h>
 #include <si.h>
 #include <cgs.h>
